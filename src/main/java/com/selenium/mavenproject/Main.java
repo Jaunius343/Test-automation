c3204b2dc581028596105361fe2db897f72c7447
Jaunius Tamulevičius <jautam@gmail.com>
2022-10-09 16:24:28 +0300

    Update Main.java
    testing if jenkins job build trigger works

--- src/main/java/com/selenium/mavenproject/Main.java
+++ src/main/java/com/selenium/mavenproject/Main.java
@@ -34,11 +34,11 @@ public class Main
 //    	Lab3_2 lab3_2 = new Lab3_2();
 //    	lab3_2.run();
     	
-    	Lab4_User create_user = new Lab4_User();
-    	User user = create_user.run();
-    	
-    	Lab4_Test lab4_test = new Lab4_Test();
-    	lab4_test.run(user, "data2.txt");
+//    	Lab4_User create_user = new Lab4_User();
+//    	User user = create_user.run();
+//    	
+//    	Lab4_Test lab4_test = new Lab4_Test();
+//    	lab4_test.run(user, "data2.txt");
     	
 
     }
